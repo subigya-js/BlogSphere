d68945f4b50daa71a3d71d94775ea6d9fcbf63cd
subigya-js <developer.subigya@gmail.com>
2025-03-11 10:58:32 +0530

    feat: add mongoose for database connection and implement connection logic

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
-const dotenv = require("dotenv");
+require("dotenv").config();
 const express = require("express");
 const { errorHandler } = require("./middleware/errorHandler");
+const connectDB = require("./config/dbConnection");
 const app = express();
 const port = process.env.PORT || 3001;
+
+connectDB();
 
 app.use(express.json());
 
