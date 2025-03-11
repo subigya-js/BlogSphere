41319da094018171ad3058e904f5a72621c32548
subigya-js <developer.subigya@gmail.com>
2025-03-11 11:23:40 +0530

    feat: add semver and jsonwebtoken libraries with necessary files and configurations

--- server/controller.js/userController.js
+++ server/controller.js/userController.js
@@ -1,6 +1,8 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/UserModel");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 // @desc Register a new user
 // @route POST /register
@@ -44,9 +46,29 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route POST /login
 // @access Public
 const loginUser = asyncHandler(async (req, res) => {
-  //   const { email, password } = req.body;
-  //   res.status(200).json({ email, password });
-  res.json({ message: "Login a user" });
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("Please fill in all fields");
+  }
+
+  const user = await User.findOne({ email });
+
+  // Compare password
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+        expiresIn: "30d",
+      }),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid email or password");
+  }
 });
 
 // @desc Current user
